85f0fa299ac064fdcf6b5a6dbb4a0945ea2216fc
dungmanh88 <luke.nothingness@gmail.com>
2016-12-18 19:10:32 +0700

    Create install_ansible.sh

---
+++ ansible/install_ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e 
+
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+yum -y install epel-release \
+&& yum -y install ansible git gcc gcc-c++ python-devel MariaDB-devel openssl-devel
+
