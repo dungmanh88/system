86decece780a7af29e039a3a5acf08ea03c13d75
dungmanh88 <luke.nothingness@gmail.com>
2016-12-21 10:15:09 +0700

    Update

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -5,5 +5,13 @@ set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 yum -y install epel-release \
-&& yum -y install ansible git gcc gcc-c++ python-devel MariaDB-devel openssl-devel
+&& yum -y install ansible git gcc gcc-c++ python-devel mariadb-devel openssl-devel wget python-cffi
+
+### support ansible vault
+cd /tmp
+if [ ! -f /tmp/get-pip.py ]; then
+    wget https://bootstrap.pypa.io/get-pip.py 
+fi
+python get-pip.py \
+&& pip install cryptography
 
