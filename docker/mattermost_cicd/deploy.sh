fee8bb46694a494afd086b006b0c4e2a5d107566
dungmanh88 <luke.nothingness@gmail.com>
2017-06-26 17:52:52 +0700

    add

--- docker/mattermost_cicd/deploy.sh
+++ docker/mattermost_cicd/deploy.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 host=$1
 port=$2
+dockerComposeVersion=1.14.0
 
 ssh ${host} -p ${port} mkdir -p /webapp/mattermost
 
@@ -10,6 +11,9 @@ rsync -avzP -e "ssh -p ${port}" docker-compose.yaml ${host}:/webapp/mattermost
 # Deploy via ssh
 ssh ${host} -p ${port} <<EOF
     set -e
+    curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    export PATH=$PATH:/usr/local/bin
     cd /webapp/mattermost
     docker-compose up
 EOF
