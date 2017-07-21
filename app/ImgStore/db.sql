4c0f9cad73ef1dbac5fc4e82c52ee0cef0cdc49b
dungmanh88 <luke.nothingness@gmail.com>
2017-07-21 17:48:01 +0700

    Init Laravel

--- app/ImgStore/db.sql
+++ app/ImgStore/db.sql
@@ -2,10 +2,12 @@ CREATE DATABASE IF NOT EXISTS album;
 
 USE album;
 
+DROP TABLE IF EXISTS image;
+
 CREATE TABLE IF NOT EXISTS image(
-  ima_name INT AUTO_INCREMENT PRIMARY KEY,
+  img_name INT AUTO_INCREMENT PRIMARY KEY,
   img_description VARCHAR(30),
   img_size VARCHAR(30),
   img_type VARCHAR(30),
   img_data LONGBLOB
-)
+);
